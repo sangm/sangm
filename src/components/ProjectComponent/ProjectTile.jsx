2aca9458a3c80d4ec111e9bf7917afff8dec0eb8
Sang Mercado <sang.mercado@gmail.com>
2015-12-27 20:37:38 -0600

    Feature: Links for ProjectTile looks great

--- src/components/ProjectComponent/ProjectTile.jsx
+++ src/components/ProjectComponent/ProjectTile.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
-import { IconButton, GridTile, Dialog } from 'material-ui'
-import { Avatar, Card, CardHeader, CardMedia, CardTitle, CardActions, FlatButton, CardText } from 'material-ui'
+import { IconButton, GridTile, Dialog, RaisedButton } from 'material-ui'
+import { Avatar, Card, CardHeader, CardMedia, CardTitle, CardActions, FlatButton, CardText, FontIcon } from 'material-ui'
 
 export default class ProjectTile extends React.Component {
 
@@ -30,7 +30,13 @@ export default class ProjectTile extends React.Component {
         const imageStyle = {width: '100%'},
             dialogStyle = {padding: 0},
             overlayStyle = {background: 'rgba(52, 152, 219, 0.4)'},
-            tileStyle = {cursor: 'pointer'};
+            tileStyle = {cursor: 'pointer'},
+            buttonStyle = {verticalAlign: 'middle'},
+            cardContentStyle = {textAlign: 'center'},
+            primaryStyle = {color: 'rgb(255, 64, 129)'},
+            secondaryStyle = {color: '#00bcd4'},
+            disabledStyle = {color: 'rgba(0,0,0,0.3)'},
+            iconStyle = {paddingLeft: '-.3em', paddingRight: '.3em', verticalAlign: 'middle'};
 
         const iconButton =
             <IconButton iconClassName="fa fa-github white"
@@ -39,12 +45,38 @@ export default class ProjectTile extends React.Component {
             />;
 
         const cardButtons = [
-            <FlatButton label="Github"
-                        disabled={!github}/>,
-            <FlatButton label="YouTube"
-                        disabled={!youtube}/>,
-            <FlatButton label="Live Demo"
-                        disabled={!demo}/>
+            <FlatButton linkButton={true}
+                        href={youtube}
+                        disabled={!youtube}
+                        style={buttonStyle}
+                        primary={true}
+                        label="YouTube">
+                <FontIcon className="fa fa-youtube"
+                          style={youtube ? Object.assign(primaryStyle, iconStyle) :
+                                           Object.assign(disabledStyle, iconStyle)}/>
+            </FlatButton>,
+
+            <FlatButton linkButton={true}
+                        href={demo}
+                        disabled={!demo}
+                        style={buttonStyle}
+                        secondary={true}
+                        label="Live Demo">
+                <FontIcon className="fa fa-laptop"
+                          style={demo ? iconStyle :
+                                           Object.assign(disabledStyle, iconStyle)}/>
+            </FlatButton>,
+
+            <FlatButton linkButton={true}
+                        href={github}
+                        style={buttonStyle}
+                        disabled={!github}
+                        secondary={true}
+                        label="GitHub">
+                <FontIcon className="fa fa-github"
+                          style={github ? Object.assign(secondaryStyle, iconStyle) :
+                                          Object.assign(disabledStyle, iconStyle)}/>
+            </FlatButton>
         ];
 
         return (
@@ -56,12 +88,10 @@ export default class ProjectTile extends React.Component {
                     onRequestClose={this.handleClose}>
 
                     <Card initiallyExpanded={true}>
-                        <CardMedia
-                            overlay={<CardTitle title={project.title} />}
-                            overlayContentStyle={overlayStyle}>
+                        <CardMedia>
                             <img src={image}/>
                         </CardMedia>
-                        <CardActions>
+                        <CardActions style={cardContentStyle}>
                             {cardButtons}
                         </CardActions>
                     </Card>
@@ -77,7 +107,7 @@ export default class ProjectTile extends React.Component {
                     cols={cols}
                     rows={rows}>
                     <img src={project.image}
-                         style={imageStyle}/>>
+                         style={imageStyle}/>
                 </GridTile>
             </div>
         )
